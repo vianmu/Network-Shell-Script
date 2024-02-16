9092a962da4f28b5305decac59c43c84df6bc54d
vianmu <vivian_murillo8@outlook.es>
2024-02-16 16:16:33 -0500

    New Feature added to display table

--- networkScript.sh
+++ networkScript.sh
@@ -30,3 +30,28 @@ echo "4.  DNS resolution"
 nslookup www.google.com.co
 
 echo "---------------------"
+
+# check open ports on local Machine
+read -p "Enter the IP Address or hostname to check open ports on: " remote_host
+echo "Checking Open Ports on $remote_host"
+nmap -p- $remote_host
+echo "-------------------"
+
+ # Traceroute to a remote Host
+ read -p "Enter the IP Address or hortname to trace route to: "trace_host
+ echo " Trace route to : $trace_host"
+ tracert $trace_host
+ echo "-----------------------------------"
+
+
+ # Test Banwidth using Speed test Cli for windows
+ read -p "Do you want to test nbandwith with speedtest CLI? (y/n): " speedtest_option
+
+ if["speedtest_option" == "y"]; then
+ ./speedtestbyookla_x64
+
+ fi
+echo "-----------------------------"
+
+
+echo " Network testing Completed"
