e20c1d2a834f4db1b91b52e13dd1df634e36061a
vianmu <vivian_murillo8@outlook.es>
2024-02-16 14:32:38 -0500

    configuracion

---
+++ networkScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Network testing script for git bash on Windows
+
+echo "Network Testing Script"
+
+#Check Internet Connectivity
+
+echo "1. Chechinh Internet Conectivity"
+
+ping -c 4 www.google.com
+
+echo "-----------------------"
+
+
